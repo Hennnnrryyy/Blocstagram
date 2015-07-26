01170d44c6a1619ee4aa8ece31de5c594313b07f
Henry <henrysmithjr@me.com>
2015-07-26 11:44:38 -0400

    Checkpoint 27 completed,building Blocstagram

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Henry Smith on 7/26/15.
+//  Copyright (c) 2015 Henry Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+
+@end
