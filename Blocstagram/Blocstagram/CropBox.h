2a0cfd8f318f124f19ee7da7e7aaafd6381b7680
Henry <henrysmithjr@me.com>
2015-08-14 18:33:41 -0400

    Checkpoint 42 completed, access to image library

---
+++ Blocstagram/Blocstagram/CropBox.h
@@ -0,0 +1,13 @@
+//
+//  CropBox.h
+//  Blocstagram
+//
+//  Created by Henry Smith on 8/14/15.
+//  Copyright (c) 2015 Henry Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CropBox : UIView
+
+@end
