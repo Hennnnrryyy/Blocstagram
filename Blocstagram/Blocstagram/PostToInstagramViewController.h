da68dff2d1a15037ecd832468024547ad4d04e42
Henry <henrysmithjr@me.com>
2015-08-19 01:00:39 -0400

    Checkpoint 43 completed, added filters

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Henry Smith on 8/18/15.
+//  Copyright (c) 2015 Henry Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
